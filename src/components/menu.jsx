3152ff4d3c6b33571be083a7ff31c2b688de00fd
Tiago Carmo <contato@tiagocarmo.com.br>
2021-03-03 13:36:42 -0300

    fix: new menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -12,19 +12,64 @@ const Menu = () => (
       Anunciantes
       <ul>
         <li>
-          <a href='/anunciantes/bob-s-chicken-crispy-cheddar'>
-            Bob`s
-          </a>
+          <a href='/anunciantes/dentro-da-historia-r-30-00'>Dentro da Historia</a>
         </li>
         <li>
-          <a href='/anunciantes/bookplay-30-dias-r9'>
-            Bookplay
-          </a>
+          <a href='/anunciantes/vittude-consulta-desconto-r80'>Vittude</a>
         </li>
         <li>
-          <a href='/anunciantes/primepass-singleday2d'>
-            Primepass
-          </a>
+          <a href='/anunciantes/petitebox-desconto-r20'>Petitebox</a>
+        </li>
+        <li>
+          <a href='/anunciantes/kinopop-assinatura-mensal'>Kinopop</a>
+        </li>
+        <li>
+          <a href='/anunciantes/bookplay-30-dias-r9'>Bookplay</a>
+        </li>
+        <li>
+          <a href='/anunciantes/bookplay-kids-16d'>Bookplay Kids</a>
+        </li>
+        <li>
+          <a href='/anunciantes/deezer-assinatura-mensal'>Deezer</a>
+        </li>
+        <li>
+          <a href='/anunciantes/curso-online-de-ingles'>Curso Online de Ingles</a>
+        </li>
+        <li>
+          <a href='/anunciantes/qualifica-cursos-plano-mensal'>Qualifica</a>
+        </li>
+        <li>
+          <a href='/anunciantes/primepass-singleday2d'>Primepass</a>
+        </li>
+        <li>
+          <a href='/anunciantes/cabify-desconto-5-reais-em-1-corrida'>Cabify</a>
+        </li>
+        <li>
+          <a href='/anunciantes/bennu-fluid-yoga-quinzenal-r10'>Bennu - Yoga</a>
+        </li>
+        <li>
+          <a href='/anunciantes/looke-filmes-e-series-10dias'>Looke</a>
+        </li>
+        <li>
+          <a href='/anunciantes/descomplica-reforco-exatas-mensal'>Descomplica - Reforco Exatas</a>
+        </li>
+        <li>
+          <a href='/anunciantes/bob-s-chicken-crispy-cheddar'>Bob`s - Chicken Crispy Cheddar</a>
+        </li>
+        <li>
+          <a href='/anunciantes/bob-s-casquinha-recheada'>Bob`s - Casquinha Recheada</a>
+        </li>
+        <li>
+          <a href='/anunciantes/bob-s-milk-shake'>Bob`s - Milk Shake</a>
+        </li>
+        <li>
+          <a href='/anunciantes/2-casquinhas-do-bob-s'>Bob`s - 2 Casquinhas</a>
+        </li>
+        <li>
+          <a href='/anunciantes/begamer-conteudo-de-jogos-mensal'>Begamer</a>
+        </li>
+        <li>
+          <a href='/anunciantes/nuuvem-xbox-live-desconto-r10'>Nuuvem - Xbox live</a>
         </li>
       </ul>
     </li>
